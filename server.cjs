a1b295e767c3d2bd5d5eed291300946efff462c2
walaeddine riahi <127868628+alpha00012@users.noreply.github.com>
2023-12-28 23:40:02 +0100

    Préparation pour déploiement sur Heroku

--- server.cjs
+++ server.cjs
@@ -8,37 +8,36 @@ app.use(cors());
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_USER, // Assurez-vous que cette variable d'environnement est définie
+        pass: process.env.EMAIL_PASS  // Assurez-vous que cette variable d'environnement est définie
+    }
 });
 
 app.post('/send-email', (req, res) => {
-  const { fullName, email, role, message } = req.body;
-  const mailOptions = {
-    from: email,
-    to: 'walaeddine.riahi@ieee.org',
-    subject: `New ${role} registration from ${fullName}`,
-    text: `Message: ${message}\nName: ${fullName}\nEmail: ${email}\nRole: ${role}`
-  };
+    const { fullName, email, role, message } = req.body;
+    const mailOptions = {
+        from: email,  // Expéditeur du courriel
+        to: 'walaeddine.riahi@ieee.org',  // Destinataire du courriel
+        subject: `New ${role} registration from ${fullName}`, // Sujet du courriel
+        text: `Message: ${message}\nName: ${fullName}\nEmail: ${email}\nRole: ${role}` // Corps du courriel
+    };
 
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res.status(500).json({ error: error.toString() });
-    }
-    res.status(200).json({ message: 'Email sent', info });
-  });
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return res.status(500).json({ error: error.toString() });
+        }
+        res.status(200).json({ message: 'Email sent', info });
+    });
 });
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3001; // Le port sur lequel votre serveur s'exécute
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur mon serveur Express !');
+    res.send('Bienvenue sur mon serveur Express !');
 });
-app.use(express.static('public'));
+app.use(express.static('public')); // Dossier pour les fichiers statiques
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
-
