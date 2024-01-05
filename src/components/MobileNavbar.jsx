56c2f1c2a8c817057bc94cabf7115af35a1f3364
walaeddine riahi <127868628+alpha00012@users.noreply.github.com>
2024-01-05 23:37:42 +0100

    exhibiting mobile

--- src/components/MobileNavbar.jsx
+++ src/components/MobileNavbar.jsx
@@ -22,6 +22,7 @@ const MobileNavbar = ({ setShowNav }) => {
             <div className="flex flex-col items-center gap-4 relative text-2xl font-bold">
             <Link to="/">Home</Link>
                     <Link to="/aboutus" >About us</Link>
+                    <Link to="/exhibiting" >Exhibiting</Link>
                     <button onClick={toggleAgendaLinks} className="flex flex-col items-center gap-4 relative text-2xl font-bold">
                     Agenda
                 </button>
